2101bd48c96141d3fb9e7a843a6b924ae35dd50d
hongsy41-netizen <genspark_dev@genspark.ai>
2026-01-17 11:29:53 +0000

    docs: README 업데이트 - 난이도 3 문제 추가 및 최신 기능 반영

---
+++ seoyul-math-pwa/check_stats.js
@@ -0,0 +1,6 @@
+// 템플릿 통계를 콘솔에 출력
+const Templates_1_1 = require('./js/generator/templates_1_1.js');
+const Templates_1_2 = require('./js/generator/templates_1_2.js');
+
+// 하지만 브라우저 환경이므로 직접 확인
+console.log('템플릿 통계 확인: /check_templates.html 페이지를 열어주세요.');
